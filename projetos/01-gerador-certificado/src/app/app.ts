88b0ab5d8be6bb4e4eecb4884143d68a1e998133
leirisson <103783610+leirisson@users.noreply.github.com>
2025-11-14 00:52:14 -0400

    iniciando projeto angular

---
+++ projetos/01-gerador-certificado/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './_components/navbar/navbar';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Navbar],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('01-gerador-certificado');
+}
